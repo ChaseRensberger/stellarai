df8a1f94fd61326a5d5f8acdec26d939d911a11b
CHR <chasehrensberger@gmail.com>
2022-07-27 02:03:42 -0400

    initial commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,35 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import {
+	ColorScheme,
+	ColorSchemeProvider,
+	MantineProvider,
+} from '@mantine/core';
+import Head from 'next/head';
+import { useState } from 'react';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+const App = ({ Component, pageProps }: AppProps) => {
+	const [colorScheme, setColorScheme] = useState<ColorScheme>('light');
+	const toggleColorScheme = (value?: ColorScheme) =>
+		setColorScheme(value || (colorScheme === 'dark' ? 'light' : 'dark'));
 
-export default MyApp
+	return (
+		<>
+			<Head>
+				<title>StellarAI</title>
+				<meta name="description" content="StellarAI" />
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
+			<ColorSchemeProvider
+				colorScheme={colorScheme}
+				toggleColorScheme={toggleColorScheme}
+			>
+				<MantineProvider withGlobalStyles withNormalizeCSS>
+					<Component {...pageProps} />
+				</MantineProvider>
+			</ColorSchemeProvider>
+		</>
+	);
+};
+
+export default App;
---
+++ pages/jobs/index.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from 'next';
+
+const Jobs: NextPage = () => {
+	return (
+		<main>
+			<div>Job Page</div>
+		</main>
+	);
+};
+
+export default Jobs;
