dda9571d2d1c5ec8988e62f69649fbd6bf84ffff
CHR <chasehrensberger@gmail.com>
2022-08-09 16:47:20 -0400

    add morning greeting

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,24 @@
 import { createStyles, Stack } from '@mantine/core';
 import type { NextPage } from 'next';
+import { useEffect, useState } from 'react';
 import { DropzoneButton } from '../components/profile/Dropzone';
 import { UserInfoIcons } from '../components/profile/UserInfo';
 
 const Profile: NextPage = () => {
 	const { classes } = useStyles();
+	const myDate = new Date();
+	const [greet, setGreet] = useState('');
+	useEffect(() => {
+		let hrs = myDate.getHours();
+		if (hrs < 12) setGreet('Good Morning');
+		else if (hrs >= 12 && hrs <= 17) setGreet('Good Afternoon');
+		else if (hrs >= 17 && hrs <= 24) setGreet('Good Evening');
+	}, []);
 
 	return (
 		<main>
 			<Stack align={'center'} spacing="xl">
-				<h1>Good Morning!</h1>
+				<h1>{greet}</h1>
 				<UserInfoIcons
 					avatar={''}
 					name={'John Smith'}
