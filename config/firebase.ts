ef9c261e292050bdebbd834ccfe5d33e144aec41
CHR <chasehrensberger@gmail.com>
2022-08-15 18:13:25 -0400

    remove bad firebase cache

--- config/firebase.ts
+++ config/firebase.ts
@@ -19,18 +19,18 @@ export const auth = getAuth(app);
 export const db = getFirestore(app);
 export const storage = getStorage(app);
 
-try {
-	console.log('enable index persistence');
-	enableIndexedDbPersistence(db).catch((err) => {
-		if (err.code == 'failed-precondition') {
-			// Multiple tabs open, persistence can only be enabled
-			// in one tab at a a time.
-			// ...
-		} else if (err.code == 'unimplemented') {
-			// The current browser does not support all of the
-			// features required to enable persistence
-			// ...
-		}
-	});
-	// Subsequent queries
-} catch (e) {}
+// try {
+// 	console.log('enable index persistence');
+// 	enableIndexedDbPersistence(db).catch((err) => {
+// 		if (err.code == 'failed-precondition') {
+// 			// Multiple tabs open, persistence can only be enabled
+// 			// in one tab at a a time.
+// 			// ...
+// 		} else if (err.code == 'unimplemented') {
+// 			// The current browser does not support all of the
+// 			// features required to enable persistence
+// 			// ...
+// 		}
+// 	});
+// 	// Subsequent queries
+// } catch (e) {}
--- next.config.js
+++ next.config.js
@@ -2,11 +2,6 @@
 const nextConfig = {
 	reactStrictMode: true,
 	swcMinify: true,
-	firestore: {
-		enablePersistence: true,
-		// emulatorPort: process.env.NODE_ENV !== 'production' ? 8080 : undefined,
-		// emulatorHost: 'localhost'
-	},
 };
 
 module.exports = nextConfig;
